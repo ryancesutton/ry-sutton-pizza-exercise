9cd4faf54dc6b835e9e8fa063c2e2c8f0594cc7c
Ry Sutton <ryancesutton@gmail.com>
2020-08-10 09:17:06 -0500

    trying different method using file stored in project, having general difficulty parsing into useful format

--- src/main/java/pizzaExercise.java
+++ src/main/java/pizzaExercise.java
@@ -1,46 +1,68 @@
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.objectweb.asm.Type;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class pizzaExercise {
 
 
-    public static void main(String[] args) {
-        try {
-            URL url = new URL("https://www.olo.com/pizzas.json");
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            String str = "";
-            while (null != (str = br.readLine())) {
-//                System.out.println(str);
+//    public static void main(String[] args) {
+//        try {
+//            URL url = new URL("https://www.olo.com/pizzas.json");
+//            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+//            StringBuilder jsonString = new StringBuilder();
+//            String str = "";
+//            while (null != (str = br.readLine())) {
+////                System.out.println(str);
+//                jsonString.append(str);
+//
+//                JSONParser parser = new JSONParser();
+//                JSONArray jsonArray = (JSONArray) parser.parse(jsonString.toString());
+//
+//                System.out.println(jsonArray.get(1));
+//
+//
+//
+//            }
+//
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//
+//    }
+public static void main(String[] args) throws Exception
+{
+    // parsing file "JSONExample.json"
+    Object obj = new JSONParser().parse(new FileReader("src/main/resources/pizza.json"));
 
-                JSONParser parser = new JSONParser();
-                JSONObject jsonObject = (JSONObject) parser.parse(str);
-                JSONArray toppings = (JSONArray) jsonObject.get("toppings");
+    // typecasting obj to JSONObject
+    JSONObject jo = (JSONObject) obj;
 
-                Iterator<String> iterator = toppings.iterator();
-                while (iterator.hasNext()) {
-                    System.out.println(iterator.next());
-                }
 
-            }
+    // getting toppings
+    Map toppings = ((Map)jo.get("toppings"));
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
+    // iterating toppings Map
+    Iterator<Map.Entry> itr1 = toppings.entrySet().iterator();
+    while (itr1.hasNext()) {
+        Map.Entry pair = itr1.next();
+        System.out.println(pair.getKey() + " : " + pair.getValue());
     }
+
+
+}
+
+
 }
