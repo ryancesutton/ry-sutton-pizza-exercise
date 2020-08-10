8fd1748d782285cbc4e9410ff2ffe8309fb8d7f2
Ry Sutton <ryancesutton@gmail.com>
2020-08-09 22:32:50 -0500

    working on converting data to JSONArray

--- src/main/java/pizzaExercise.java
+++ src/main/java/pizzaExercise.java
@@ -1,19 +1,43 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.objectweb.asm.Type;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class pizzaExercise {
 
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         try {
             URL url = new URL("https://www.olo.com/pizzas.json");
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             String str = "";
             while (null != (str = br.readLine())) {
-                System.out.println(str);
+//                System.out.println(str);
+
+                JSONParser parser = new JSONParser();
+                JSONObject jsonObject = (JSONObject) parser.parse(str);
+                JSONArray toppings = (JSONArray) jsonObject.get("toppings");
+
+                Iterator<String> iterator = toppings.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
+
             }
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
